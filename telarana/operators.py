0937d64c6714adee6e8628774319f65f2bd60917
ckat609 <armando@armandotello.com>
2021-04-10 01:32:10 -0500

    add cloth and material

--- telarana/operators.py
+++ telarana/operators.py
@@ -4,6 +4,18 @@ import math
 import numpy
 from functools import reduce
 
+bl_info = {
+    "name": "Telaraña",
+    "author": "Armando Tello, Will Wright",
+    "description": "",
+    "blender": (2, 92, 0),
+    "version": (0, 0, 1),
+    "location": "",
+    "description": "Generates a cobweb using the current grease pencil layer strokes",
+    "warning": "",
+    "category": "Object"
+}
+
 
 def getThreadRoots(strokes):
     s1, s2 = random.sample(list(strokes), k=2)
@@ -85,8 +97,9 @@ def createConnectingThreads(threads, threadCount, threadSteps):
     for j in range(threadCount):
         l1, l2 = random.sample(threads, k=2)
 
-        randVert1 = random.randrange(1, len(l1['verts']) - 1)
-        randVert2 = random.randrange(1, len(l2['verts']) - 1)
+        firstLastVert = 1
+        randVert1 = random.randrange(firstLastVert, len(l1['verts']) - firstLastVert)
+        randVert2 = random.randrange(firstLastVert, len(l2['verts']) - firstLastVert)
         p1 = l1['verts'][randVert1]
         p2 = l2['verts'][randVert2]
 
@@ -95,6 +108,14 @@ def createConnectingThreads(threads, threadCount, threadSteps):
 
     return connectedThreads
 
+
+# def createThreadsRecursively(threads, threadCount, threadSteps, count):
+#     if(count <= 0):
+#         return threads
+
+#     newThreads = createConnectingThreads(threads, threadCount, threadSteps)
+
+#     return threads + createThreadsRecursively(newThreads, threadCount, threadSteps, count-1)
 
 def createThreadsRecursively(threads, threadCount, threadSteps, count):
     if(count <= 0):
@@ -115,36 +136,89 @@ def createMainThreads(strokes, threadCount, threadSteps):
     return mainThreads
 
 
+def createTelaranaObject(threadCount, threadSteps, threadConnectionCount, threadConnectionSteps, recursionsLevesl):
+    layers = bpy.context.scene.grease_pencil.layers
+    strokes = layers.active.active_frame.strokes
+
+    mainThreads = createMainThreads(strokes, threadCount, threadSteps)
+    connectedThreads = createThreadsRecursively(mainThreads, threadConnectionCount, threadConnectionSteps, recursionsLevesl)
+
+    allThreads = mainThreads + connectedThreads
+    mesh = reduce(processThreads, allThreads, {'verts': [], 'edges': [], 'pins': []})
+    connectedMesh = connectThreads(mesh)
+
+    meshTelarana = bpy.data.meshes.new("Telarana")
+    objTelarana = bpy.data.objects.new("Telarana", meshTelarana)
+    bpy.context.collection.objects.link(objTelarana)
+
+    objTelarana.vertex_groups.new(name='pins')
+
+    meshTelarana.from_pydata(connectedMesh['verts'], connectedMesh['edges'], [])
+    meshTelarana.update()
+
+    objTelarana.vertex_groups['pins'].add(connectedMesh['pins'], 1.0, 'ADD')
+
+    return objTelarana
+
+
+def addClothModifier(obj, shrink):
+    mod = obj.modifiers.new('Telarana', 'CLOTH')
+    mod.settings.vertex_group_mass = "pins"
+    mod.settings.shrink_min = shrink   # -0.3 --- 0.3
+    mod.settings.bending_model = 'LINEAR'
+
+    return mod
+
+
+def addMaterial(obj):
+    mat = bpy.data.materials.get('Telarana')
+    print(mat)
+    if mat is None:
+        mat = bpy.data.materials.new('Telarana')
+    obj.data.materials.append(mat)
+
+    if mat.node_tree.nodes.get('ShaderNodeBsdfTranslucent') is not None:
+        mat.use_nodes = True
+        mat.node_tree.nodes.remove(mat.node_tree.nodes.get('Principled BSDF'))
+        matOut = mat.node_tree.nodes.get('Material Output')
+        translucent = mat.node_tree.nodes.new('ShaderNodeBsdfTranslucent')
+        translucent.inputs['Color'].default_value = (1, 1, 1, 1)
+        mat.node_tree.links.new(matOut.inputs[0], translucent.outputs[0])
+
+    return mat
+
+
 class CreateTelaranaOperator(bpy.types.Operator):
     bl_idname = "object.create_telarana"
     bl_label = "Create Telarana"
 
     def execute(self, context):
-        THREAD_COUNT = 15
+        THREAD_COUNT = 10
         THREAD_STEPS = 10
-        THREAD_CONNECTIONS_COUNT = 50
-        THREAD_CONNECTIONS_STEPS = 10
-        RECURSION_LEVELS = 5
+        THREAD_CONNECTIONS_COUNT = 5
+        THREAD_CONNECTIONS_STEPS = 5
+        RECURSION_LEVELS = 100
 
-        layers = bpy.context.scene.grease_pencil.layers
-        strokes = layers.active.active_frame.strokes
+        SHRINK_FACTOR = 0.3
+        BEVEL_DEPTH = 0.0003
 
-        mainThreads = createMainThreads(strokes, THREAD_COUNT, THREAD_STEPS)
-        connectedThreads = createThreadsRecursively(mainThreads, THREAD_CONNECTIONS_COUNT, THREAD_CONNECTIONS_STEPS, RECURSION_LEVELS)
+        obj = createTelaranaObject(THREAD_COUNT, THREAD_STEPS, THREAD_CONNECTIONS_COUNT, THREAD_CONNECTIONS_STEPS, RECURSION_LEVELS)
+        addClothModifier(obj, SHRINK_FACTOR)
+        addMaterial(obj)
 
-        allThreads = mainThreads + connectedThreads
-        mesh = reduce(processThreads, allThreads, {'verts': [], 'edges': [], 'pins': []})
-        connectedMesh = connectThreads(mesh)
-
-        meshTelarana = bpy.data.meshes.new("Telarana")
-        objTelarana = bpy.data.objects.new("Telarana", meshTelarana)
-        bpy.context.collection.objects.link(objTelarana)
-
-        objTelarana.vertex_groups.new(name='pins')
-
-        meshTelarana.from_pydata(connectedMesh['verts'], connectedMesh['edges'], [])
-        meshTelarana.update()
-
-        objTelarana.vertex_groups['pins'].add(connectedMesh['pins'], 1.0, 'ADD')
+        # bpy.context.view_layer.objects.active = obj
+        # obj.select_set(True)
+        # bpy.ops.object.convert(target='CURVE')
+        # bpy.context.object.data.bevel_depth = BEVEL_DEPTH
 
         return {'FINISHED'}
+
+
+# def register():
+#     bpy.utils.register_class(CreateTelaranaOperator)
+#     # bpy.types.VIEW3D_MT_add.append(menu_func)
+
+
+# def unregister():
+#     bpy.utils.unregister_class(CreateTelaranaOperator)
+#     # bpy.types.VIEW3D_MT_add.remove(menu_func)
